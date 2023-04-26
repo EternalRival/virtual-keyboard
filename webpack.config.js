18af562f27a016fceee9470b0fb8b1ca011bc655
EternalRival <eternalrival8@gmail.com>
2023-04-26 11:39:19 +0300

    feat: add webpack&eslint

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: { filename: 'main.js', clean: true },
+  mode: 'production',
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: 'src/index.html', favicon: 'src/assets/favicon.svg' }),
+  ],
+  module: {
+    rules: [{ test: /\.scss$/i, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] }],
+  },
+};
