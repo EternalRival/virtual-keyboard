890952f7e20dda80c7249925f4d5377cd68d422e
EternalRival <eternalrival8@gmail.com>
2023-05-01 19:39:09 +0300

    feat: implement arrow keys

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,19 +1,18 @@
 import { Component } from './Component';
-import { emitter } from '../service/emitter';
 
 export class Key extends Component {
   constructor(keyCode, keyData, className, parent) {
     super('button', `${className} ${keyCode}`, {}, parent);
     [this.code, this.data] = [keyCode, keyData];
-    this.node.addEventListener('click', () => emitter.emit(this.code, this));
   }
 
   get value() {
     return this.node.value;
   }
 
-  setKey(locale, isShifted) {
-    this.node.value = this.data.locale[locale][isShifted ? 'shifted' : 'value'];
+  setKey(locale, { shift, caps }) {
+    const newValue = this.data.locale[locale][shift ? 'shifted' : 'default'];
+    this.node.value = caps ? newValue[shift ? 'toLowerCase' : 'toUpperCase']() : newValue;
   }
 
   keydown(e) {
