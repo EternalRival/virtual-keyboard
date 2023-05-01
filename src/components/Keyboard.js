112e12d433ccaca3ff16de189a3b54f4b203b884
EternalRival <eternalrival8@gmail.com>
2023-05-01 19:39:09 +0300

    feat: implement arrow keys

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -4,6 +4,8 @@ import { Component } from './Component';
 import { Key } from './Key';
 
 export class Keyboard extends Component {
+  state = { shift: 0, caps: 0 };
+
   constructor(textField, props, parent) {
     super('ul', 'keyboard', props, parent);
     this.textField = textField;
@@ -11,20 +13,48 @@ export class Keyboard extends Component {
     this.keys = [...KeyMap].map(([keyCode, keyData]) => {
       const item = new Component('li', 'keyboard__item', null, this.node);
       const key = new Key(keyCode, keyData, 'keyboard__key', item.node);
-      key.setKey(Keyboard.locale);
       return { item, key };
     });
 
+    this.renderLayout();
     this.init();
   }
 
   init() {
     window.addEventListener('keydown', (e) => this.getKey(e.code)?.keydown(e));
     window.addEventListener('keyup', (e) => this.getKey(e.code)?.keyup(e));
+
+    const handlers = {
+      Backspace: () => this.textField.useAction('backspace'),
+      Delete: () => this.textField.useAction('delete'),
+      Tab: () => this.textField.useAction('tab'),
+      Enter: () => this.textField.useAction('enter'),
+      CapsLock: () => {
+        this.caps = !this.caps;
+        this.renderLayout(this.caps);
+      },
+      ShiftLeft: () => {
+        this.shift = !this.shift;
+        this.renderLayout(this.shift);
+      },
+      ShiftRight: () => {},
+      ControlLeft: () => {},
+      ControlRight: () => {},
+      AltLeft: () => {},
+      AltRight: () => {},
+      MetaLeft: () => {},
+      ArrowUp: () => this.textField.useAction('up'),
+      ArrowLeft: () => this.textField.useAction('left'),
+      ArrowDown: () => this.textField.useAction('down'),
+      ArrowRight: () => this.textField.useAction('right'),
+      // default: (e) => this.textField.insertText(e.target.value),
+      default: (e) => this.textField.useAction('insert', e.target.value),
+    };
+    this.keys.forEach(({ key: { code, node } }) => node.addEventListener('click', handlers[code] ?? handlers.default));
   }
 
-  renderLayout(isShifted) {
-    this.keys.forEach(({ key }) => key.setKey(Keyboard.locale, isShifted));
+  renderLayout() {
+    this.keys.forEach(({ key }) => key.setKey(Keyboard.locale, this.state));
   }
 
   switchLocale() {
--- src/components/TextField.js
+++ src/components/TextField.js
@@ -2,14 +2,63 @@ import { Component } from './Component';
 
 export class TextField extends Component {
   constructor(className, props, parent) {
-    super('textarea', className || 'text-field', { autofocus: true, ...{ rows: 5, cols: 60 }, ...props }, parent);
+    super('textarea', className || 'text-field', { autofocus: true, ...{ rows: 4, cols: 60 }, ...props }, parent);
   }
 
-  insertText(text) {
-    const { node } = this;
-    const { selectionStart, selectionEnd, value } = node;
-    node.focus();
-    node.value = `${value.slice(0, selectionStart)}${text}${value.slice(selectionEnd)}`;
-    node.selectionEnd = selectionStart + text.length;
+  useAction(name, ...args) {
+    this[name](...args);
+    this.node.blur();
+    this.node.focus();
+  }
+
+  insert(text) {
+    const { selectionStart, selectionEnd, value } = this.node;
+    const [start, end] = [selectionStart, selectionEnd];
+    this.node.value = `${value.slice(0, start)}${text}${value.slice(end)}`;
+    this.node.selectionEnd = start + text.length;
+  }
+
+  enter() {
+    this.insert('\n');
+  }
+
+  tab() {
+    this.insert('\t');
+  }
+
+  backspace() {
+    const { selectionStart, selectionEnd, value } = this.node;
+    const [start, end] = [selectionStart - (selectionStart === selectionEnd), selectionEnd];
+    this.node.value = `${value.slice(0, start)}${value.slice(end)}`;
+    this.node.selectionEnd = start;
+  }
+
+  delete() {
+    const { selectionStart, selectionEnd, value } = this.node;
+    const [start, end] = [selectionStart, selectionEnd + (selectionStart === selectionEnd)];
+    this.node.value = `${value.slice(0, start)}${value.slice(end)}`;
+    this.node.selectionEnd = start;
+  }
+
+  left() {
+    this.node.selectionStart -= 1;
+    this.node.selectionEnd = this.node.selectionStart;
+  }
+
+  right() {
+    this.node.selectionEnd += 1;
+    this.node.selectionStart = this.node.selectionEnd;
+  }
+
+  up() {
+    const { selectionStart, cols, value } = this.node;
+    this.node.selectionEnd = Math.max(value.lastIndexOf('\n', selectionStart - 1), selectionStart - cols - 1, 0);
+    this.node.selectionStart = this.node.selectionEnd;
+  }
+
+  down() {
+    const { cols, selectionEnd, value } = this.node;
+    this.node.selectionEnd = Math.min(value.indexOf('\n', selectionEnd + 1), selectionEnd + cols + 1, value.length);
+    this.node.selectionStart = this.node.selectionEnd;
   }
 }
