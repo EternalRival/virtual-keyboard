46ba4b95ab6969bf32ada8bad55159743061b349
EternalRival <eternalrival8@gmail.com>
2023-05-10 01:30:12 +0300

    refactor: extract inputfield actions to Map

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -6,54 +6,44 @@ export class InputField extends Component {
   }
 
   useAction(name, ...args) {
-    this[name](...args);
+    this.actions.get(name)(...args);
     this.node.blur();
     this.node.focus();
   }
 
-  insert(text) {
-    const { selectionStart, selectionEnd } = this.node;
-    this.node.setRangeText(text, selectionStart, selectionEnd, 'end');
-  }
-
-  enter() {
-    this.insert('\n');
-  }
-
-  tab() {
-    this.insert('\t');
-  }
-
-  backspace() {
-    const { selectionStart, selectionEnd } = this.node;
-    if (!selectionEnd) return;
-    this.node.setRangeText('', selectionStart - (selectionStart === selectionEnd), selectionEnd, 'end');
-  }
-
-  delete() {
-    const { selectionStart, selectionEnd } = this.node;
-    this.node.setRangeText('', selectionStart, selectionEnd + (selectionStart === selectionEnd), 'end');
-  }
-
-  left() {
-    this.node.selectionStart -= 1;
-    this.node.selectionEnd = this.node.selectionStart;
-  }
-
-  right() {
-    this.node.selectionEnd += 1;
-    this.node.selectionStart = this.node.selectionEnd;
-  }
-
-  up() {
-    const { selectionStart, cols, value } = this.node;
-    this.node.selectionEnd = Math.max(value.lastIndexOf('\n', selectionStart - 1), selectionStart - cols - 1, 0);
-    this.node.selectionStart = this.node.selectionEnd;
-  }
-
-  down() {
-    const { cols, selectionEnd, value } = this.node;
-    this.node.selectionEnd = Math.min(value.indexOf('\n', selectionEnd + 1), selectionEnd + cols + 1, value.length);
-    this.node.selectionStart = this.node.selectionEnd;
-  }
+  actions = new Map([
+    ['insert', (text) => {
+      const { selectionStart, selectionEnd } = this.node;
+      this.node.setRangeText(text, selectionStart, selectionEnd, 'end');
+    }],
+    ['enter', () => this.insert('\n')],
+    ['tab', () => this.insert('\t')],
+    ['backspace', () => {
+      const { selectionStart, selectionEnd } = this.node;
+      if (!selectionEnd) return;
+      this.node.setRangeText('', selectionStart - (selectionStart === selectionEnd), selectionEnd, 'end');
+    }],
+    ['delete', () => {
+      const { selectionStart, selectionEnd } = this.node;
+      this.node.setRangeText('', selectionStart, selectionEnd + (selectionStart === selectionEnd), 'end');
+    }],
+    ['left', () => {
+      this.node.selectionStart -= 1;
+      this.node.selectionEnd = this.node.selectionStart;
+    }],
+    ['right', () => {
+      this.node.selectionEnd += 1;
+      this.node.selectionStart = this.node.selectionEnd;
+    }],
+    ['up', () => {
+      const { selectionStart, cols, value } = this.node;
+      this.node.selectionEnd = Math.max(value.lastIndexOf('\n', selectionStart - 1), selectionStart - cols - 1, 0);
+      this.node.selectionStart = this.node.selectionEnd;
+    }],
+    ['down', () => {
+      const { cols, selectionEnd, value } = this.node;
+      this.node.selectionEnd = Math.min(value.indexOf('\n', selectionEnd + 1), selectionEnd + cols + 1, value.length);
+      this.node.selectionStart = this.node.selectionEnd;
+    }],
+  ]);
 }
